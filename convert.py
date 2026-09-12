159931809b63074c23c38bc461bf33055f010501
rahul2614 <rv261457@gmail.com>
2026-09-12 18:04:04 +0530

    chore: cleanup temporary scripts

--- convert.py
+++
@@ -1,5 +0,0 @@
-from svglib.svglib import svg2rlg
-from reportlab.graphics import renderPM
-
-drawing = svg2rlg('public/icons/favicon.svg')
-renderPM.drawToFile(drawing, 'public/icons/logo_highres.png', fmt='PNG', dpi=300)
--- fix_logo_text.cjs
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const file = 'src/components/common/Text2HandwritingLogo.tsx';
-let content = fs.readFileSync(file, 'utf8');
-
-content = content.replace(/Ink<span/g, 'Text<span');
-content = content.replace(/>Trail<\/span>/g, '>2Handwriting</span>');
-
-fs.writeFileSync(file, content, 'utf8');
